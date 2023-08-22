9a19b8d05a36404bb19afbabfec8321f1a40d775
at350 <46613976+at350@users.noreply.github.com>
2023-08-21 20:19:57 -0700

    testing

--- USACO/2017jan/Silver/CowDanceShow.java
+++ USACO/2017jan/Silver/CowDanceShow.java
@@ -1,8 +1,8 @@
 // USACO 2017 January Contest, Silver
 // Problem 1. Cow Dance Show
 // link: http://www.usaco.org/index.php?page=viewproblem2&cpid=690
-// status: ac
-// tag:
+// status: AC
+// tag: binary search
 
 import java.util.*;
 import java.io.*;
